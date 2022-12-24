d7d8f6740df668152747fb46cd7df103805ffd30
notjik <notjik@yandex.ru>
2022-12-24 14:24:10 +0500

    update CHANGE-TITLE.py

--- CHANGE-TITLE.py
+++ CHANGE-TITLE.py
@@ -6,4 +6,4 @@ def repl(s: str) -> str:
     return re.sub(r'\([^)]*\)', '', s).lower().strip().replace(' ', '-')
 
 
-[os.rename(f, repl(f)) for f in os.listdir('.') if not f.startswith('.')]
+[os.rename(f, repl(f)) for f in os.listdir('.') if os.path.splitext(f)[-1] == '.py' and f != os.path.basename(__file__)]
